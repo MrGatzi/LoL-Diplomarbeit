ee387a51b8e647d861b5c73587d29b5c4c00b771
MrGatzi <martingatzweiler@gmail.com>
2015-06-30 20:40:44 +0200

    Version 0.4
    Added the Request for the "Recent-Games"
    added Request to Cache

--- PHP/Cache_and_API_Request.php
+++ PHP/Cache_and_API_Request.php
@@ -29,7 +29,6 @@
 			//Check if Requested Data is already in Cache
 			$SumObj = $cache->get("$Input_RequestData[SumName_input]_$Input_RequestData[ServerName_input]_IDRequest");
 			if($SumObj == null) {
-				fwrite($fh,"Does not Exist in Data Base | ");
 				$url="https://$Input_RequestData[ServerName_input].api.pvp.net/api/lol/euw/v1.4/summoner/by-name/$Input_RequestData[SumName_input]?api_key=fbe18d9e-025e-4b0a-a71a-c4844cbd4850";
 				// CURL Request on Inputs
 				$ch = curl_init();
@@ -39,37 +38,36 @@
 				$result=curl_exec($ch);
 				curl_close($ch);
 				// Decode Json String If Summoner don't exist Eror!
-				if($SumObj = json_decode($result, true)){
-					fwrite($fh,"Sucess Created : ");
-					fwrite($fh,$SumObj[$Input_RequestData['SumName_input']]['name']);
+				if($SumObj = json_decode($result)){
+					fwrite($fh,$SumObj->{$Input_RequestData['SumName_input']}->name);
 				}else{
 					fwrite($fh,"Error Sumoner doesn't Exist |");
 				};
 				// Save Data in Cache for 1 Minute
 				$cache->set("$Input_RequestData[SumName_input]_$Input_RequestData[ServerName_input]_IDRequest",$SumObj , 60);
-				fwrite($fh," Wrote it in Database| ");
 			}else{
-				fwrite($fh,"Does (!) Exist in Data Base : ");
-				$SumObj=json_encode($SumObj, true);
-				echo $SumObj;
-			}
-			// BUG !! --> Wenn der SUmmoner noch nicht im Cach ist Fehler ....
+				//print_r ($SumObj);
+			};
 			// Get Recent Games Played
-			$SumObj=json_decode($SumObj);
-			$test1=$SumObj->{$Input_RequestData['SumName_input']}->id;
-			$url="https://$Input_RequestData[ServerName_input].api.pvp.net/api/lol/euw/v1.3/game/by-summoner/$test1/recent?api_key=fbe18d9e-025e-4b0a-a71a-c4844cbd4850";
-			// CURL Request on Inputs
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_URL,$url);
-			$result=curl_exec($ch);
-			curl_close($ch);
-			$result=json_decode($result);
-			$Return->SumGames=$result;
-			print_r($Return->SumGames->games[0]->gameMode);
-			fclose($fh);
+			$Return->SumGames = $cache->get("$Input_RequestData[SumName_input]_$Input_RequestData[ServerName_input]_RecentGames");
+			if($Return->SumGames==NULL){
+				$SumObj=json_encode($SumObj);// ohne dem gehts ned warum auch immer
+				$SumObj=json_decode($SumObj);
+				$test1=$SumObj->{$Input_RequestData['SumName_input']}->id;
+				$url="https://$Input_RequestData[ServerName_input].api.pvp.net/api/lol/euw/v1.3/game/by-summoner/$test1/recent?api_key=fbe18d9e-025e-4b0a-a71a-c4844cbd4850";
+				// CURL Request on Inputs
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_URL,$url);
+				$result=curl_exec($ch);
+				curl_close($ch);
+				$result=json_decode($result);
+				$Return->SumGames=$result;
+				$cache->set("$Input_RequestData[SumName_input]_$Input_RequestData[ServerName_input]_RecentGames",$Return->SumGames , 60);
+			}
+			$Return->SumInfo=$SumObj;			
+			echo json_encode($Return);
 		}else{
-			//echo Leer Leer
 		}
 ?>

a683be3112a65d651a7336154b021f54d68a60ff
MrGatzi <martingatzweiler@gmail.com>
2015-07-13 12:18:17 +0200

    Verion 0.6.2
    Bugfix

--- JS/Controller.js
+++ JS/Controller.js
@@ -4,17 +4,17 @@ var data;
 	// function if button "hit-it" is pressed
      $("#Hit_it").button().on("click", function() {
 		event.preventDefault();
-		var Data1 = {SumName_input:$('#SumName').val().toLowerCase(), ServerName_input:$('#ServName').val().toLowerCase()};
+		var Data1 = {SumName_input:$('.SumName').val().toLowerCase(), ServerName_input:$('#ServName').val().toLowerCase()};
 		var check =$.post("PHP/Cache_and_API_Request.php",{Data1});
 		check.done(function(response) {
 			console.log( "success_new" );
 			// Test-Subjekt to show some data
 			var show=JSON.parse(response);
-			var test1=$('#SumName').val().toLowerCase();
+			var test1=$('.SumName').val().toLowerCase();
 			$( "#show_test" ).append("Your SummonerID is : "+show.SumInfo[test1].id+"\n");
 			$( "#show_test" ).append("The Last Game ID : "+show.SumGames.games[1].gameId+"\n");
 			$( "#show_test" ).append("The Last Game you Played was an : "+show.SumGames.games[1].gameMode+"\n");
-			$( "#show_test" ).append("<a href='GameOverview.php?SumName="+$('#SumName').val().toLowerCase()+"&SumServer="+$('#ServName').val().toLowerCase()+"&GameId="+show.SumGames.games[1].gameId+"'>Game1</a>");
+			$( "#show_test" ).append("<a href='GameOverview.php?SumName="+$('.SumName').val().toLowerCase()+"&SumServer="+$('#ServName').val().toLowerCase()+"&GameId="+show.SumGames.games[1].gameId+"'>Game1</a>");
 			
 		})
 		check.fail(function() {

68befc197d430a116ed0ec1397fa6a0f05423453
FabioPernegger <fabio.pernegger@24speed.at>
2015-07-14 10:06:19 +0200

    0.7.1
    moved contoller code to Model

--- JS/Controller.js
+++ JS/Controller.js
@@ -3,22 +3,6 @@ var data;
    $(document).ready(function() {
 	// function if button "hit-it" is pressed
      $("#Hit_it").button().on("click", function() {
-		event.preventDefault();
-		var Data1 = {SumName_input:$('.SumName').val().toLowerCase(), ServerName_input:$('#ServName').val().toLowerCase()};
-		var check =$.post("PHP/Cache_and_API_Request.php",{Data1});
-		check.done(function(response) {
-			console.log( "success_new" );
-			// Test-Subjekt to show some data
-			var show=JSON.parse(response);
-			var test1=$('.SumName').val().toLowerCase();
-			$( "#show_test" ).append("Your SummonerID is : "+show.SumInfo[test1].id+"\n");
-			$( "#show_test" ).append("The Last Game ID : "+show.SumGames.games[1].gameId+"\n");
-			$( "#show_test" ).append("The Last Game you Played was an : "+show.SumGames.games[1].gameMode+"\n");
-			$( "#show_test" ).append("<a href='GameOverview.php?SumName="+$('.SumName').val().toLowerCase()+"&SumServer="+$('#ServName').val().toLowerCase()+"&RecentGame=0'>Game1</a>");
-			
-		})
-		check.fail(function() {
-			console.log( "Error while sendig to the database to create Data" );
-		})
+		getData();
 	});
 });

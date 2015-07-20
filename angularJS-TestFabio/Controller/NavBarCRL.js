173eac40634ac491c4b45bf5d3eb69c9965426e8
MrGatzi <martingatzweiler@gmail.com>
2015-07-20 13:39:40 +0200

    0.3.6

--- angularJS-TestFabio/Controller/NavBarCRL.js
+++ angularJS-TestFabio/Controller/NavBarCRL.js
@@ -6,10 +6,11 @@ sampleApp.controller('NavBarCRL',['$scope', 'mySharedService', '$http','$window'
 	$scope.valid ={
 			'SumInfo':0,
 			'SumGames':0,
-			'new1' :0
 		};
+	flag=0;
 	// When Hit-ItButton is Pressed.
 	$scope.handleClick = function() {
+		flag=0;
 		data1 = {
             'SumName_input' : angular.lowercase($scope.SumName),
             'ServerName_input' : $scope.ServName
@@ -18,17 +19,15 @@ sampleApp.controller('NavBarCRL',['$scope', 'mySharedService', '$http','$window'
 		success(function(data, status, config) {
 			sharedService.prepForBroadcast($scope.valid);
 			$scope.valid=data;
-			//$scope.valid.new1++;
 			})
 		.error(function(data, status, headers, config) {
 			alert("fail");
 		});
 		// when the Variable $scope.valid Changed, Checked if 0. If 0 then go to _Mistake.
 		$scope.$watch('valid', function(newValue, oldValue) {
-			if (newValue !== oldValue) {
-				
+			if (newValue != oldValue && flag<1) {
+				flag++;
 				if($scope.valid.SumInfo != null){
-					console.log($scope.valid);
 					
 					$window.location.href = 'http://127.0.0.1/LoL-Diplomarbeit/angularJS-TestFabio/#/RecentGames';
 					

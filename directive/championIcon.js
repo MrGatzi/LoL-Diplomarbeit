8e41781b717cdc832402e2a056f2051a4e22cbb1
MrGatzi <martingatzweiler@gmail.com>
2015-07-30 10:29:36 +0200

    added new direktiv

--- directive/championIcon.js
+++ directive/championIcon.js
@@ -1,14 +1,7 @@
-sampleApp.directive('championIcon', function(){
-	 return {
-		restrict: 'AE',
-				// scope object defines the attributes that can be used in the HTML tag
-				// for '@', '=', '&' syntax see http://onehungrymind.com/angularjs-sticky-notes-pt-2-isolated-scope/
-        scope:{
-	      },
-        link: function(scope, element, attrs) {
-			element.bind("mouseenter", function() {
-				console.log("hallo");
-			});
-        } 
+sampleApp.directive("enter", function() {
+    return function(scope, element, attrs) {
+        element.bind("mouseenter", function() {
+            scope.$apply(attrs.enter);
+        });
     };
 });
